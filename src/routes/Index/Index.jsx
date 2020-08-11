b296223dd57563b61aee33960a2382b3fd620882
Vitaly Rtishchev <rtivital@gmail.com>
2020-08-11 10:23:41 +0300

    [index] Add new logo

--- src/routes/Index/Index.jsx
+++ src/routes/Index/Index.jsx
@@ -4,6 +4,7 @@ import Footer from '../../components/Footer/Footer';
 import GithubButton from '../../components/GithubButton/GithubButton';
 import Container from '../../components/Container/Container';
 import toolsData from '../../data/tools';
+import logoText from '../../assets/logo-text.svg';
 import classes from './Index.styles.less';
 
 export default function Index() {
@@ -16,7 +17,7 @@ export default function Index() {
       <Container>
         <header className={classes.header}>
           <div className={classes.headerBody}>
-            <h1 className={classes.title}>Omatsuri</h1>
+            <img className={classes.logo} src={logoText} alt="" />
             <p className={classes.description}>Open source browser tools for everyday use</p>
           </div>
           <GithubButton />
