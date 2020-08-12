f3e522cc60a3aeecb1deb15cb3b549ccd52a322b
Vitaly Rtishchev <rtivital@gmail.com>
2020-08-12 16:06:38 +0300

    [lorem-ipsum] Add basic settings controls

--- src/routes/lorem-ipsum/LoremIpsum.jsx
+++ src/routes/lorem-ipsum/LoremIpsum.jsx
@@ -4,12 +4,17 @@ import generateText from './generate-text';
 import Settings from './Settings/Settings';
 import Preview from './Preview/Preview';
 
+const INITIAL_VALUES = {
+  length: 3,
+  type: 'lorem',
+};
+
 export default function LoremIpsum() {
   const ls = useLocalStorage({ key: '@omatsuri/lorem-ipsum', delay: 1000 });
-  const initialValues = ls.retrieve();
+  const initialValues = ls.retrieve() || INITIAL_VALUES;
 
-  const [type, setType] = useState((initialValues && initialValues.type) || 'pokemon');
-  const [length, setLength] = useState(3);
+  const [type, setType] = useState(initialValues.type || 'lorem');
+  const [length, setLength] = useState(initialValues.length || 3);
   const [text, setText] = useState(generateText(type, length));
 
   useEffect(() => {
--- src/routes/lorem-ipsum/Settings/Settings.jsx
+++ src/routes/lorem-ipsum/Settings/Settings.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import SliderInput from '../../../components/SliderInput/SliderInput';
+import Tabs from '../../../components/Tabs/Tabs';
+import { AVAILABLE_GENERATORS } from '../generate-text';
 import Background from '../../../components/Background/Background';
 import classes from './Settings.styles.less';
 
-export default function Settings() {
-  return <Background className={classes.wrapper}>Settings</Background>;
+const types = AVAILABLE_GENERATORS.map(generator => ({
+  value: generator,
+  label: `${generator.charAt(0).toUpperCase() + generator.slice(1)} ipsum`,
+}));
+
+export default function Settings({ onTypeChange, type, length, onLengthChange }) {
+  return (
+    <Background className={classes.wrapper}>
+      <Tabs data={types} active={type} onTabChange={onTypeChange} />
+      <div className={classes.footer}>
+        <div className={classes.length}>
+          <div className={classes.label}>Amount of paragraphs</div>
+          <SliderInput value={length} min={1} max={20} onChange={onLengthChange} />
+        </div>
+      </div>
+    </Background>
+  );
 }
