2c532ecb8c239de78a0d61f0c52fe0fd612782a1
Vitaly Rtishchev <rtivital@gmail.com>
2020-08-14 15:52:35 +0300

    [symbols] Add css data to symbols

---
+++ src/data/symbols/index.js
@@ -0,0 +1,5 @@
+export basic from './basic-symbols';
+export fractions from './fractions-symbols';
+export math from './math-symbols';
+export punctuation from './punctuation-symbols';
+export special from './special-symbols';
