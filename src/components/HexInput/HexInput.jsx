48d1d0fa79fa9d3f68d5553fe795ba1a7f434ed4
Vitaly Rtishchev <rtivital@gmail.com>
2020-08-16 20:53:43 +0300

    [core] Add color picker to hex input

--- src/components/HexInput/HexInput.jsx
+++ src/components/HexInput/HexInput.jsx
@@ -1,28 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
+import { ChromePicker as ColorPicker } from 'react-color';
 import Input from '../Input/Input';
 import useIntermediateValue from '../../hooks/use-intermediate-value';
+import classes from './HexInput.styles.less';
 
-export default function HexInput({ value, onChange, ...others }) {
+export default function HexInput({ className, value, onChange, ...others }) {
+  const [colorPickerValue, setColorPickerValue] = useState(value);
   const { intermediateValue, valid, handleChange, handleSubmit } = useIntermediateValue({
     value,
     onChange,
-    rule: val => /^#[0-9A-F]{6}$/i.test(val),
+    rule: (val) => /^#[0-9A-F]{6}$/i.test(val),
   });
 
+  useEffect(() => {
+    setColorPickerValue(value);
+  }, [value]);
+
   return (
-    <Input
-      {...others}
-      invalid={!valid}
-      type="text"
-      value={intermediateValue}
-      onChange={event => handleChange(event.target.value)}
-      onBlur={event => handleSubmit(event.target.value)}
-    />
+    <div className={cx(classes.wrapper, className)}>
+      <Input
+        {...others}
+        invalid={!valid}
+        type="text"
+        value={intermediateValue}
+        onChange={(event) => handleChange(event.target.value)}
+        onBlur={(event) => handleSubmit(event.target.value)}
+      />
+      <div className={classes.pickerWrapper}>
+        <ColorPicker
+          className={classes.picker}
+          color={colorPickerValue}
+          onChange={(event) => setColorPickerValue(event.hex)}
+          onChangeComplete={(event) => handleChange(event.hex)}
+        />
+      </div>
+    </div>
   );
 }
 
 HexInput.propTypes = {
+  className: PropTypes.string,
   value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
 };
