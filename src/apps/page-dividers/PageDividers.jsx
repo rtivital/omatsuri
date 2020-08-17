063c01f0082750486da061f5d1318afcec6ee691
Vitaly Rtishchev <rtivital@gmail.com>
2020-08-17 15:48:15 +0300

    [page-dividers] Add localstorage logic

--- src/apps/page-dividers/PageDividers.jsx
+++ src/apps/page-dividers/PageDividers.jsx
@@ -1,17 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import useLocalStorage from '../../hooks/use-local-storage';
 import useDocumentTitle from '../../hooks/use-document-title';
 import Settings from './Settings/Settings';
 import Preview from './Preview/Preview';
 
+const INITIAL_VALUES = {
+  type: 'waves_opaque',
+  direction: 'normal',
+  position: 'top',
+  width: 125,
+  height: 75,
+  color: '#ffffff',
+};
+
 export default function PageDividers() {
   useDocumentTitle('Curved page dividers');
 
-  const [type, setType] = useState('waves');
-  const [color, setColor] = useState('#ffffff');
-  const [width, setWidth] = useState(125);
-  const [height, setHeight] = useState(75);
-  const [position, setPosition] = useState('top');
-  const [direction, setDirection] = useState('normal');
+  const ls = useLocalStorage({ key: '@omatsuri/page-dividers', delay: 1000 });
+  const initialValues = ls.retrieve() || INITIAL_VALUES;
+
+  const [type, setType] = useState(initialValues.type);
+  const [color, setColor] = useState(initialValues.color);
+  const [width, setWidth] = useState(initialValues.width);
+  const [height, setHeight] = useState(initialValues.height);
+  const [position, setPosition] = useState(initialValues.position);
+  const [direction, setDirection] = useState(initialValues.direction);
+
+  useEffect(() => {
+    ls.save({ direction, width, height, color, position, type });
+  }, [direction, width, height, color, position, type]);
 
   const values = { type, color, width, height, position, direction };
   const handlers = {
