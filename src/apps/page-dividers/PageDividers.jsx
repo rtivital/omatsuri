17066365e792113ed60887d9f30bb69281201a5a
Vitaly Rtishchev <rtivital@gmail.com>
2020-08-17 15:11:25 +0300

    [page-dividers] Add full settings logic

--- src/apps/page-dividers/PageDividers.jsx
+++ src/apps/page-dividers/PageDividers.jsx
@@ -6,10 +6,21 @@ export default function PageDividers() {
   useDocumentTitle('Curved page dividers');
 
   const [type, setType] = useState('waves');
+  const [color, setColor] = useState('#ffffff');
+  const [width, setWidth] = useState(125);
+  const [height, setHeight] = useState(75);
 
   return (
     <div>
-      <Settings values={{ type }} handlers={{ onTypeChange: setType }} />
+      <Settings
+        values={{ type, color, width, height }}
+        handlers={{
+          onTypeChange: setType,
+          onColorChange: setColor,
+          onWidthChange: setWidth,
+          onHeightChange: setHeight,
+        }}
+      />
     </div>
   );
 }
--- src/apps/page-dividers/Settings/Settings.jsx
+++ src/apps/page-dividers/Settings/Settings.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import SliderInput from '../../../components/SliderInput/SliderInput';
 import Select from '../../../components/Select/Select';
+import HexInput from '../../../components/HexInput/HexInput';
 import Background from '../../../components/Background/Background';
 import { shapes } from '../Shape/Shape';
 import classes from './Settings.styles.less';
@@ -13,23 +15,59 @@ const data = Object.keys(shapes).map((shape) => ({
 export default function Settings({ values, handlers }) {
   return (
     <Background className={classes.wrapper}>
-      <Select
-        data={data}
-        value={values.type}
-        onChange={handlers.onTypeChange}
-        id="shape-select"
-        label="Shape type"
-      />
+      <div className={classes.inner}>
+        <div className={classes.group}>
+          <Select
+            data={data}
+            value={values.type}
+            onChange={handlers.onTypeChange}
+            id="shape-select"
+            label="Shape type"
+          />
+        </div>
+        <div className={classes.group}>
+          <div className={classes.label}>Color</div>
+          <HexInput value={values.color} onChange={handlers.onColorChange} />
+        </div>
+
+        <div className={classes.group}>
+          <div className={classes.input}>
+            <div className={classes.label}>Width, %</div>
+            <SliderInput
+              value={values.width}
+              onChange={handlers.onWidthChange}
+              min={100}
+              max={400}
+            />
+          </div>
+
+          <div className={classes.input}>
+            <div className={classes.label}>Height, px</div>
+            <SliderInput
+              value={values.height}
+              onChange={handlers.onHeightChange}
+              min={0}
+              max={500}
+            />
+          </div>
+        </div>
+      </div>
     </Background>
   );
 }
 
 Settings.propTypes = {
   values: PropTypes.shape({
+    color: PropTypes.string.isRequired,
     type: PropTypes.oneOf(Object.keys(shapes)).isRequired,
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired,
   }),
 
   handlers: PropTypes.shape({
     onTypeChange: PropTypes.func.isRequired,
+    onColorChange: PropTypes.func.isRequired,
+    onWidthChange: PropTypes.func.isRequired,
+    onHeightChange: PropTypes.func.isRequired,
   }).isRequired,
 };
