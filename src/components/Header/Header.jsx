f9cd4460b8cffbe06e417344253ff06fdf416880
Vitaly Rtishchev <rtivital@gmail.com>
2020-08-17 20:15:23 +0300

    [core] Add home link to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import GithubButton from '../GithubButton/GithubButton';
 import logoText from '../../assets/logo-text.svg';
 import classes from './Header.styles.less';
@@ -7,7 +8,9 @@ export default function Header() {
   return (
     <header className={classes.header}>
       <div className={classes.headerBody}>
-        <img className={classes.logo} src={logoText} alt="" />
+        <Link to="/">
+          <img className={classes.logo} src={logoText} alt="" />
+        </Link>
         <p className={classes.description}>Open source browser tools for everyday use</p>
       </div>
       <GithubButton />
