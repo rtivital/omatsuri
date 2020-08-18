455dba89d1b67327f5bffd98ef3fcd35c4cd14d6
Vitaly Rtishchev <rtivital@gmail.com>
2020-08-18 10:45:22 +0300

    [color-shades-generator] Add values saving to local storage

--- src/apps/color-shades-generator/ColorShadesGenerator.jsx
+++ src/apps/color-shades-generator/ColorShadesGenerator.jsx
@@ -1,8 +1,9 @@
 import oc from 'open-color';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { v4 } from 'uuid';
-import Button from '../../components/Button/Button';
+import useLocaStorage from '../../hooks/use-local-storage';
 import useDocumentTitle from '../../hooks/use-document-title';
+import Button from '../../components/Button/Button';
 import ColorShadesList from './ColorShadesList/ColorShadesList';
 import Settings from './Settings/Settings';
 import classes from './ColorShadesGenerator.styles.less';
@@ -14,12 +15,21 @@ const defaultPalette = Object.keys(oc)
     color: oc[key][4],
   }));
 
+const INITIAL_VALUES = {
+  value: [{ color: '#ffffff', key: v4() }],
+  saturation: 0.2,
+  darken: 0.1,
+};
+
 export default function ColorShadesGenerator() {
   useDocumentTitle('Color shades generator');
 
-  const [value, setValue] = useState([{ color: '#ffffff', key: v4() }]);
-  const [saturation, setSaturation] = useState(0.2);
-  const [darken, setDarken] = useState(0.1);
+  const ls = useLocaStorage({ key: '@omatsuri', delay: 1000 });
+  const initialValues = ls.retrieve() || INITIAL_VALUES;
+
+  const [value, setValue] = useState(initialValues.value);
+  const [saturation, setSaturation] = useState(initialValues.saturation);
+  const [darken, setDarken] = useState(initialValues.darken);
   const canDelete = value.length > 1;
 
   const addColor = () => setValue((current) => [...current, { color: '#ffffff', key: v4() }]);
@@ -40,6 +50,10 @@ export default function ColorShadesGenerator() {
 
   const removeAll = () => setValue([{ color: '#ffffff', key: v4() }]);
   const loadDefaultPalette = () => setValue(defaultPalette);
+
+  useEffect(() => {
+    ls.save({ value, saturation, darken });
+  }, [value, saturation, darken]);
 
   const colors = value.map((color, index) => (
     <ColorShadesList
