6eeffee54469ce227870580732c30923b86df986
Vitaly Rtishchev <rtivital@gmail.com>
2020-08-18 11:40:39 +0300

    [core] Fix useDocumentTitle hook to update title on initial render

--- src/hooks/use-document-title.js
+++ src/hooks/use-document-title.js
@@ -1,7 +1,7 @@
-import { useEffect } from 'react';
+import { useLayoutEffect } from 'react';
 
 export default function useDocumentTitle(title) {
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (typeof document !== 'undefined') {
       document.title = title;
     }
