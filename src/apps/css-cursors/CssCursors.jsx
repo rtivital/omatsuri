bf8f241d8e92e2a961f89c995673c686ff5ab4ff
Vitaly Rtishchev <rtivital@gmail.com>
2020-08-27 14:22:17 +0300

    [css-cursors] Add full app logic

--- src/apps/css-cursors/CssCursors.jsx
+++ src/apps/css-cursors/CssCursors.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import Background from '../../components/Background/Background';
+import SettingsLabel from '../../components/SettingsLabel/SettingsLabel';
+import CursorControl from './CursorControl/CursorControl';
+import data from './data';
+import classes from './CssCursors.styles.less';
 
 export default function CssCursors() {
-  return <div>CssCursors</div>;
+  const controls = data.map((value) => (
+    <CursorControl key={value} value={value} className={classes.control} />
+  ));
+
+  return (
+    <Background className={classes.wrapper}>
+      <SettingsLabel className={classes.title}>
+        Css cursor properties – click button to copy property value
+      </SettingsLabel>
+
+      <div className={classes.controls}>{controls}</div>
+    </Background>
+  );
 }
---
+++ src/apps/css-cursors/CursorControl/CursorControl.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import useClipboard from '../../../hooks/use-clipboard';
+import data from '../data';
+import classes from './CursorControl.styles.less';
+
+export default function CursorControl({ className, value }) {
+  const clipboard = useClipboard();
+  return (
+    <button
+      className={cx(classes.control, { [classes.active]: clipboard.copied }, className)}
+      type="button"
+      onClick={() => clipboard.copy(value)}
+      style={{ cursor: value }}
+    >
+      {value}
+    </button>
+  );
+}
+
+CursorControl.propTypes = {
+  className: PropTypes.string,
+  value: PropTypes.oneOf(data).isRequired,
+};
