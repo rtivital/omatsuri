8d3fd90900d7fe20c99b20fec7b43027d175f28b
Vitaly Rtishchev <rtivital@gmail.com>
2020-09-01 11:18:50 +0300

    [core] Fix missing document title on home page

--- src/pages/index/Index.jsx
+++ src/pages/index/Index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import useDocumentTitle from '../../hooks/use-document-title';
 import ToolCard from '../../components/ToolCard/ToolCard';
 import PageBase from '../../components/PageBase/PageBase';
 import toolsData from '../../data/tools';
 import classes from './Index.styles.less';
 
 export default function Index() {
+  useDocumentTitle('Omatsuri');
+
   const tools = toolsData.map((tool) => (
     <ToolCard className={classes.tool} key={tool.name} {...tool} />
   ));
