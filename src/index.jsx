d2c3e0a2e5b048852745c488a7fe7e401229aa04
Vitaly Rtishchev <rtivital@gmail.com>
2020-09-20 20:01:38 +0300

    [core] Remove styles folder

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,9 @@
+import 'normalize.css';
+import 'react-colorful/dist/index.css';
+
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
-import './styles/global';
+import './styles.less';
 
 render(<App />, document.getElementById('app'));
--- src/styles/global.js
+++
@@ -1,3 +0,0 @@
-import 'normalize.css';
-import 'react-colorful/dist/index.css';
-import './global-styles.less';
