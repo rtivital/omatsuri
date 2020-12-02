346868738e73c9c45d6170640a7ebf4ce7ccd3a4
Vitaly Rtishchev <rtivital@gmail.com>
2020-12-02 12:18:04 +0300

    [core] Remove some jsx-a11y eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,11 @@ module.exports = {
     node: true,
   },
 
+  rules: {
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+  },
+
   settings: {
     'import/resolver': {
       node: {},
