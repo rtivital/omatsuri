dadc1197633d00845ccaae817840bfb9ce40ba36
Vitaly Rtishchev <rtivital@gmail.com>
2020-12-07 09:51:07 +0300

    [css-cursors] Add dark theme support

--- src/apps/css-cursors/CursorControl/CursorControl.jsx
+++ src/apps/css-cursors/CursorControl/CursorControl.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import { useClipboard } from 'xooks';
+import { useTheme } from '../../../ThemeProvider';
 import classes from './CursorControl.styles.less';
 
 export default function CursorControl({ className, value }) {
+  const [theme] = useTheme();
   const clipboard = useClipboard();
+
   return (
     <button
-      className={cx(classes.control, { [classes.active]: clipboard.copied }, className)}
+      className={cx(
+        classes.control,
+        { [classes.active]: clipboard.copied },
+        classes[theme],
+        className
+      )}
       type="button"
       onClick={() => clipboard.copy(value)}
       style={{ cursor: value }}
